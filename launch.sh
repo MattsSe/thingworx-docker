16d91fa3190cdc519ae0b15104b8554f86b3b2fb
Jorge Claro <jmc.claro@gmail.com>
2017-05-21 22:39:25 +0100

    Cleanup on launch script

--- launch.sh
+++ launch.sh
@@ -1,34 +1,20 @@
 #! /bin/bash
 
-#su - postgres -c '/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf &'
-
-
-echo Starting Up!
+echo Starting Up!...
 
 function shut_down() {
-   echo shutting down
-
-   #pid=$(ps -e | grep java | awk '{print $1}')
-   #kill $pid
-
+   echo ...Shutting down!
    catalina.sh stop
-
    /etc/init.d/postgresql stop
-
    exit 0
 }
 
 trap "shut_down" SIGINT
-
 /etc/init.d/postgresql start
-
 catalina.sh start
-
 tail -f /usr/local/tomcat/logs/catalina.out &
 
 while :
 do
-   #tail -f /dev/null &
    sleep 5
 done
-
